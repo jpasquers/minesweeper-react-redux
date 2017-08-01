38cd9285887114a1a837f19407fa442f508ca19e
PASQUE <jp606j@att.com>
2017-08-01 10:53:17 -0500

    basic redux setup

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Game} from './components/Game';
+import {Settings} from './components/Settings';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Minesweeper</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container-fluid">
+          <div className="settings-container">
+            <Settings/>
+          </div>
+          <div className="game-container">
+            <Game/>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { reduce } from './reducers/base';
+import { createStore } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(reduce);
+
+ReactDOM.render(
+    <Provider store={store}> 
+        <App /> 
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
