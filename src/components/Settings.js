4463680f1f5bcd8c9a332f19394f89e6e85d0857
PASQUE <jp606j@att.com>
2017-08-07 13:17:13 -0500

    styling for settings section

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -6,15 +6,25 @@ class SettingsComponent extends Component {
   render() {
     return (
         <div className="settings-container">
-          <select value={this.props.size} onChange={this.props.handleSizeChange}>
-            <option value="5">5x5</option>
-            <option value="10">10x10</option>
-            <option value="20">20x20</option>
-            <option value="50">50x50</option>  
-          </select>
-
-          <input type="text" value={this.props.numMines} onChange={this.props.handleNumMinesChange}/>
-
+          <table>
+            <tr>
+              <td>Grid Size:</td>
+              <td>
+                <select className="form-control" value={this.props.size} onChange={this.props.handleSizeChange}>
+                  <option value="5">5 x 5</option>
+                  <option value="10">10 x 10</option>
+                  <option value="15">15 x 15</option>
+                  <option value="20">20 x 20</option>  
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td>Number Of Mines</td>
+              <td>
+                <input type="text" value={this.props.numMines} onChange={this.props.handleNumMinesChange}/>
+              </td>
+            </tr>
+          </table>
           <button onClick={this.props.resetBoard}>Update (and reset) Board</button>
         </div>
     );
